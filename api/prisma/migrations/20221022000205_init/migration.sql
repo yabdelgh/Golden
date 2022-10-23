cb06e05863c935737bd760d775492a76063c6b77
yabdelgh <yassine.abdou.1998@gmail.com>
2022-10-23 19:55:30 +0100

    install prisma, add prisma schema and migrate it

---
+++ api/prisma/migrations/20221022000205_init/migration.sql
@@ -0,0 +1,118 @@
+-- CreateEnum
+CREATE TYPE "RoomAccess" AS ENUM ('Private', 'Protected', 'Public');
+
+-- CreateEnum
+CREATE TYPE "RoomStatus" AS ENUM ('Opened', 'Closed', 'Deleted');
+
+-- CreateEnum
+CREATE TYPE "RoomUserRole" AS ENUM ('Owner', 'Admin', 'User');
+
+-- CreateEnum
+CREATE TYPE "RoomUserStatus" AS ENUM ('Invit', 'Member', 'ExMember');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "twentyFourId" TEXT NOT NULL,
+    "username" TEXT,
+    "customName" TEXT,
+    "email" TEXT,
+    "avatarUrl" TEXT,
+    "firstTime" BOOLEAN NOT NULL DEFAULT true,
+    "isTwoFactorAuthenticationEnabled" BOOLEAN NOT NULL DEFAULT false,
+    "twoFactorAuthenticationCode" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "friendships" (
+    "user1Id" INTEGER NOT NULL,
+    "user2Id" INTEGER NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "friendships_pkey" PRIMARY KEY ("user1Id","user2Id")
+);
+
+-- CreateTable
+CREATE TABLE "blocked_users" (
+    "blockerId" INTEGER NOT NULL,
+    "blockedId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "blocked_users_pkey" PRIMARY KEY ("blockerId","blockedId")
+);
+
+-- CreateTable
+CREATE TABLE "chat_rooms" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "status" "RoomStatus" NOT NULL DEFAULT 'Opened',
+    "access" "RoomAccess" NOT NULL DEFAULT 'Public',
+    "password" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "chat_rooms_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "rooms_users" (
+    "roomId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "role" "RoomUserRole" NOT NULL DEFAULT 'User',
+    "ban" BOOLEAN NOT NULL DEFAULT false,
+    "mute" BOOLEAN NOT NULL DEFAULT false,
+    "status" "RoomUserStatus" NOT NULL DEFAULT 'Member',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "rooms_users_pkey" PRIMARY KEY ("roomId","userId")
+);
+
+-- CreateTable
+CREATE TABLE "RoomUserMsg" (
+    "id" SERIAL NOT NULL,
+    "roomId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "msg" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "RoomUserMsg_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_twentyFourId_key" ON "users"("twentyFourId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "chat_rooms_name_key" ON "chat_rooms"("name");
+
+-- AddForeignKey
+ALTER TABLE "friendships" ADD CONSTRAINT "friendships_user1Id_fkey" FOREIGN KEY ("user1Id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "friendships" ADD CONSTRAINT "friendships_user2Id_fkey" FOREIGN KEY ("user2Id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "blocked_users" ADD CONSTRAINT "blocked_users_blockerId_fkey" FOREIGN KEY ("blockerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "blocked_users" ADD CONSTRAINT "blocked_users_blockedId_fkey" FOREIGN KEY ("blockedId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "rooms_users" ADD CONSTRAINT "rooms_users_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "chat_rooms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "rooms_users" ADD CONSTRAINT "rooms_users_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RoomUserMsg" ADD CONSTRAINT "RoomUserMsg_roomId_userId_fkey" FOREIGN KEY ("roomId", "userId") REFERENCES "rooms_users"("roomId", "userId") ON DELETE RESTRICT ON UPDATE CASCADE;

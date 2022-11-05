a0a86e86d722b8a4fa6e84175c6ca9f8b4c2a441
yabdelgh <yassine.abdou.1998@gmail.com>
2022-11-05 02:08:18 +0100

    create the chat provider

--- front/src/Context/ChatProvider.tsx
+++ front/src/Context/ChatProvider.tsx
@@ -1,13 +1,39 @@
-import { createContext, ProviderProps, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const ChatContext = createContext(null);
+const ChatContext = createContext<any | null>(null);
 
-const ChatProvider = ({children}: ProviderProps<unknown>) => {
-    return <ChatContext.Provider value={null}>{children}</ChatContext.Provider>
-}
+const ChatProvider = ({ children }: any) => {
+  const [user, setUser] = useState({});
+  const [chats, setChats] = useState({});
+  const [rooms, setRooms] = useState([]);
+  const [selectedRoom, setSelectedRoom] = useState([]);
+
+  const Navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("/api/user/me")
+      .then((data) => setUser(data.data))
+      .catch(() => Navigate("/"));
+    axios
+      .get("/api/chat/rooms")
+      .then((data) => setRooms(data.data))
+      .catch(() => console.log("error"));
+  }, [Navigate]);
+
+  return (
+    <ChatContext.Provider
+      value={{ user, setUser, rooms, setRooms, selectedRoom, setSelectedRoom }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
 
 export const ChatState = () => {
-    return useContext(ChatContext);
-}
+  return useContext(ChatContext);
+};
 
 export default ChatProvider;

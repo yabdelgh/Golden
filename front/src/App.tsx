c8c77cf15e11bca9f9fa4da924806524b22fdace
yabdelgh <yassine.abdou.1998@gmail.com>
2022-11-05 02:19:56 +0100

    change the min-height to 100vh instead of 150

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-import ChatPage from './Pages/ChatPage';
+import ChatPage from "./Pages/ChatPage";
 function App() {
   return (
     <div className="App">

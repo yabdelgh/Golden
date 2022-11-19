4732db897b162d93695ab8eaa547b39374125bf5
yabdelgh <yassine.abdou.1998@gmail.com>
2022-11-19 20:18:02 +0100

    add a tmp route (sos: return a token) only for test

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,16 +1,40 @@
 import { Controller, UseGuards, Get, Req, Res } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt.guard';
 import { Passport42AuthGuard } from './guards/passport42.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+    private jwtService: JwtService,
+    private configService: ConfigService
+  ) { }
 
   @Get('login')
   @UseGuards(Passport42AuthGuard)
   async login() { }
-  
+ 
+  @Get('sos')
+  async pop(@Res() res) { 
+    const id: number = 18;
+    const payload = {
+      login: 'yabdelgh',
+      sub: id,
+      email: 'yassine.abdou.1998@gmail.com',
+      imageUrl: 'undefined',
+    };
+    const token = await this.jwtService.signAsync(payload, {
+      expiresIn: '7d',
+      secret: this.configService.get<string>('JWT_SECRET'),
+    });
+    res.cookie('access_token', token, {
+      httpOnly: true,
+    });
+    res.redirect('http://localhost:3000/chat');
+  }
+
   @Get('42/callback')
   @UseGuards(Passport42AuthGuard)
   async callback(@Req() req, @Res() res) {
--- api/src/chat/chat.controller.ts
+++ api/src/chat/chat.controller.ts
@@ -2,13 +2,18 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { RoomService } from './room/room.service';
 
+interface Room {
+    [key: string]: any
+}
+
+
 @Controller('chat')
 @UseGuards(JwtAuthGuard)
 export class ChatController {
     constructor (private roomService: RoomService) { }
     
     @Get('/rooms')
-    async getMyChats(@Req() { user }) { 
+    async getMyChats(@Req() { user }: any): Promise<Room> { 
         return this.roomService.getRooms(user.id);
     }
 

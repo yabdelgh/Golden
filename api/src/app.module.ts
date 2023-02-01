229a3b4d3a19a97fb7f05e0182bc96e3b6c72c65
4yuub <karafi.work@gmail.com>
2023-02-01 21:32:20 +0100

    add route to serv avatars

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,11 +5,17 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { ChatModule } from './chat/chat.module';
 import { GameModule } from './game/game.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { avatarsDist } from './user/avatar.config';
 
 @Module({
   imports: [
     AuthModule,
     UserModule,
+    ServeStaticModule.forRoot({
+      rootPath: avatarsDist,
+      serveRoot: '/avatars'
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true

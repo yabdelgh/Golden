976f9c3b5983cc8638303883c9113f75ed31dd11
4yuub <karafi.work@gmail.com>
2023-02-02 20:45:19 +0100

    create basic structure for upload avatar component

---
+++ front/src/Components/Avatar/AvatarUpdate.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Image, Text } from "@chakra-ui/react";
+import { useState, useRef } from "react";
+
+const AvatarPreview = ({ username, link }: any) => {
+  const [avatar, serAvatar] = useState(link);
+  const avatarRef = useRef<HTMLInputElement>(null);
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      width={"350px"}
+      height={"500px"}
+      style={{ marginLeft: "500px" }}
+      fontFamily="Inter"
+    >
+      <Text fontWeight={"bold"} fontSize={"32px"} mb={"2rem"}>Change Avatar</Text>
+      <Image borderRadius="100%" boxSize="200px" src={avatar} alt={username} />
+      <Box
+        display="flex"
+        justifyContent="space-around"
+        width={"100%"}
+        mt="2rem"
+      >
+        <input type="file" hidden ref={avatarRef} />
+        <Button
+          width="70%"
+          m="1rem"
+          bg="green.300"
+          onClick={() => {
+            avatarRef.current?.click();
+          }}
+        >
+          Choose Avatar
+        </Button>
+        <Button width="70%" m="1rem" bg="green.300">
+          Save
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default AvatarPreview;

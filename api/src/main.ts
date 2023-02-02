d8a5ee6e58d061e16f7593cdd5fb26e620f95a26
4yuub <karafi.work@gmail.com>
2023-02-03 00:28:53 +0100

    enable cors in the api

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,4 +1,4 @@
-import { NestFactory } from '@nestjs/core';
+git import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { AuthenticatedSocketIoAdapter } from './AuthenticatedSocketIoAdapter';
@@ -6,6 +6,11 @@ import { AuthenticatedSocketIoAdapter } from './AuthenticatedSocketIoAdapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // setup cors
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.useWebSocketAdapter(new AuthenticatedSocketIoAdapter(app));

0dea0b6a781931f24a376b9e45bc2cb7de241443
4yuub <karafi.work@gmail.com>
2023-02-04 05:13:12 +0100

    change the style of edit avatar button

--- front/src/Components/Profile/ProfileBar.tsx
+++ front/src/Components/Profile/ProfileBar.tsx
@@ -1,60 +1,89 @@
-import { EditIcon } from "@chakra-ui/icons";
+import { PlusSquareIcon } from "@chakra-ui/icons";
 import { Box, Image, Text } from "@chakra-ui/react";
 import { AppState } from "../../Context/AppProvider";
 import ChangeAvatarModal from "../ChangeAvatarModal";
 import FriendButton from "../Buttons/FriendButton";
 import MessageButton from "../Buttons/MessageButton";
+import { useState } from "react";
 
 const ProfileBar = () => {
   const { userProfile, Friends, user } = AppState();
+  const [hiddenButton, setHiddenButton] = useState(true);
+
+  const handleMouseEnter = () => {
+    setHiddenButton(false);
+  };
+
+  const handleMouseLeave = () => {
+    setHiddenButton(true);
+  };
+
   return (
-          <Box 
-          width={{base: '100%', xl:'49%'}}
-          backgroundImage= 'linear-gradient(teal 50%, white 0%)'
-          h={{base: '600px', xl: '48%'}}
-          display="flex"
-          flexDir="column"
-          alignItems="center"
-          borderRadius='lg'
-          justifyContent={'center'}
-          mb='20px'
-          >
+    <Box
+      width={{ base: "100%", xl: "49%" }}
+      backgroundImage="linear-gradient(teal 50%, white 0%)"
+      h={{ base: "600px", xl: "48%" }}
+      display="flex"
+      flexDir="column"
+      alignItems="center"
+      borderRadius="lg"
+      justifyContent={"center"}
+      mb="20px"
+    >
       {userProfile && (
         <>
-        <Box height='70%'
-          display="flex"
-          flexDir="column"
-          alignItems="center"
-          borderRadius='lg'
-          justifyContent={'center'}
-        >
-          <Box position={"relative"}>
-            <Image
-              borderRadius="100%"
-              width="180px"
-              height="190px"
-              m="10px"
-              src={userProfile.imageUrl || "/defaultProfilePic.png"}
-            />
-            <ChangeAvatarModal username={userProfile.login} link={userProfile.imageUrl || "/defaultProfilePic.png"}>
-              <button
-                style={{ position: "absolute", bottom: "25px", right: "25px" }}
+          <Box
+            height="70%"
+            display="flex"
+            flexDir="column"
+            alignItems="center"
+            borderRadius="lg"
+            justifyContent={"center"}
+          >
+            <Box position={"relative"}>
+              {
+                <Image
+                  borderRadius="100%"
+                  width="180px"
+                  height="190px"
+                  m="10px"
+                  src={userProfile.imageUrl || "/defaultProfilePic.png"}
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                />
+              }
+              <ChangeAvatarModal
+                username={userProfile.login}
+                link={userProfile.imageUrl || "/defaultProfilePic.png"}
+              >
+                <button
+                  hidden={hiddenButton}
+                  style={{
+                    position: "absolute",
+                    bottom: "40%",
+                    right: "40%",
+                    background: "#e3e3e3aa",
+                    borderRadius: "50%",
+                    width: "40px",
+                    height: "40px",
+                  }}
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
                 >
-                <EditIcon w={7} h={7} color="black.600"/>
-              </button>
-            </ChangeAvatarModal>
+                  <PlusSquareIcon w={7} h={7} />
+                </button>
+              </ChangeAvatarModal>
+            </Box>
+            <Text fontSize="30px" color="gray.600">
+              {userProfile.login}
+            </Text>
           </Box>
-          <Text fontSize="30px" color="gray.600">
-            {userProfile.login}
-          </Text>
-          </Box>
-          { userProfile.id !== user.id && 
-
-          <Box>
-            <MessageButton target={userProfile} icon={false}/>
-            <FriendButton target={userProfile} icon={false}/>
-          </Box>  
-          }
+          {userProfile.id !== user.id && (
+            <Box>
+              <MessageButton target={userProfile} icon={false} />
+              <FriendButton target={userProfile} icon={false} />
+            </Box>
+          )}
           {/* <Box display='flex' width='40%' justifyContent={'space-around'}>
           <Box
             display="flex"
@@ -86,7 +115,7 @@ const ProfileBar = () => {
           </Box> */}
         </>
       )}
-          </Box>
+    </Box>
   );
 };
 

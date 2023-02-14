60496d8814d51cc60d0a8aac5d1c9774597f72a2
4yuub <karafi.work@gmail.com>
2023-02-14 06:14:23 +0100

    add winner component and modal

---
+++ front/src/Components/game/WinnerModal.tsx
@@ -0,0 +1,24 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    useDisclosure,
+    Box,
+  } from "@chakra-ui/react";
+import Winner from "./Winner";
+  
+  const WinnerModal = ({ winner, isOpen }: any) => {
+  
+    return (
+      <Box>
+        <Modal size="3xl" onClose={()=>{}} isOpen={isOpen ?? false} isCentered>
+          <ModalOverlay />
+          <ModalContent>
+              <Winner login={winner.login} image={winner.image} />
+          </ModalContent>
+        </Modal>
+      </Box>
+    );
+  };
+  
+  export default WinnerModal;
